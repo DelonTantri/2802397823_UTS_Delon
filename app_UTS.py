9c36f33dc8592d1c012bbc747d9a97fe9bf6dc67
DelonTantri <delontantriwijaya@gmail.com>
2026-04-19 23:55:22 +0700

    Update app_UTS.py

--- app_UTS.py
+++ app_UTS.py
@@ -91,7 +91,6 @@ def main():
     st.title("Student Placement & Salary Prediction")
     st.markdown(
         "Use the sidebar at the top left to enter student profile values, then press **Predict** to get placement probability and salary estimate."
-        "\nHasilnya di bawah bos."
     )
     st.markdown(
         "BTW hasilnya di bawah."
